3b8e114c2807a0a65771021730b79fb0efe27c64
silverwind <me@silverwind.io>
2026-07-27 09:27:20 +0200

    Pin @playwright/test
    https://github.com/microsoft/playwright/issues/41989

--- index.ts
+++ index.ts
@@ -6,7 +6,8 @@ export type CustomConfig = Config & {url: string};
 export function base(config: CustomConfig): Config {
   return deepMerge(config, {
     pin: {
-      typescript: "^6", // wait on typescript-eslint to support 7
+      "typescript": "^6", // wait on typescript-eslint to support 7
+      "@playwright/test": "<= 1.61.0", // https://github.com/microsoft/playwright/issues/41989
     },
   }, {arrayExtend: true});
 }
